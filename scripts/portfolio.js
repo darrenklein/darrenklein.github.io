b84f7fd873b408374828bd16ae05869fd32f9d08
Darren Klein <darren.a.klein@gmail.com>
2017-10-17 21:24:04 -0400

    Add Gitlab link, open source section

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,21 +1,21 @@
 function scroll(target){
-    $('html, body').animate({
+    $("html, body").animate({
         scrollTop: $(target).offset().top-160
-    }, 1000);
-};
+    }, 1000)
+}
 
-$("#about_button").click(function() {
-    scroll("#about");
-});
+$(".about_button").click(function() {
+    scroll("#about")
+})
 
-$("#projects_button").click(function() {
-    scroll("#projects");
-});
+$(".projects_button").click(function() {
+    scroll("#projects")
+})
 
-$("#press_button").click(function() {
-    scroll("#press");
-});
+$(".press_button").click(function() {
+    scroll("#press")
+})
 
-$("#contact_button").click(function() {
-    scroll("#contact");
-});
+$(".contact_button").click(function() {
+    scroll("#contact")
+})

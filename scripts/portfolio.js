14e58201ae735be8e08a18870b5e32adc726db34
Darren Klein <darren.a.klein@gmail.com>
2018-08-10 22:34:11 -0400

    Able to stop Youtube videos on modal closing.

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -19,3 +19,10 @@ $(".press_button").click(function() {
 $(".contact_button").click(function() {
     scroll("#contact")
 })
+
+// When closing a modal, stop a Youtube video from playing.
+$("[data-reveal]").on("closed.zf.reveal", function() {
+    $(".youtube_player_iframe").each(function() {
+        this.contentWindow.postMessage("{\"event\": \"command\", \"func\": \"stopVideo\", \"args\": \"\"}", "*")
+    })
+})
